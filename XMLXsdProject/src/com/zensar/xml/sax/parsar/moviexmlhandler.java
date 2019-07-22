232c85cfd09594a0616eb9376df4feb4b6424a5c
parul55 <psparulsingh4@gmail.com>
2019-07-22 17:58:23 +0530

    Added moviexmlhandler in XMLXsdProject

---
+++ XMLXsdProject/src/com/zensar/xml/sax/parsar/moviexmlhandler.java
@@ -0,0 +1,41 @@
+package com.zensar.xml.sax.parsar;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class moviexmlhandler extends DefaultHandler {
+
+	@Override
+	public void startDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		super.startDocument();
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		super.endDocument();
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		super.startElement(uri, localName, qName, attributes);
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		// TODO Auto-generated method stub
+		super.endElement(uri, localName, qName);
+	}
+
+	@Override
+	public void characters(char[] ch, int start, int length) throws SAXException {
+		// TODO Auto-generated method stub
+		for(int i=start;i<start+length;i++){
+			System.out.println(ch[i]);
+		}
+	}
+	
+}
