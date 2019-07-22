bebc20c3c1d912d458aa2a30e4411499ec3c7603
parul55 <psparulsingh4@gmail.com>
2019-07-22 18:23:54 +0530

    Added java files and full running code in XMLXsdProject

--- XMLXsdProject/src/com/zensar/xml/sax/parsar/moviexmlhandler.java
+++ XMLXsdProject/src/com/zensar/xml/sax/parsar/moviexmlhandler.java
@@ -34,7 +34,7 @@ public class moviexmlhandler extends DefaultHandler {
 	public void characters(char[] ch, int start, int length) throws SAXException {
 		// TODO Auto-generated method stub
 		for(int i=start;i<start+length;i++){
-			System.out.println(ch[i]);
+			System.out.print(ch[i]);
 		}
 		System.out.println();
 	}
